a08653bed8e4a6da637da669f47dd9b16d6f3042
Dmytro <patkovskyi@gmail.com>
2019-02-13 11:58:43 +0200

    Back to Java11, new conn strings.

--- bot/src/main/java/com/codenjoy/dojo/snakebattle/client/Main.java
+++ bot/src/main/java/com/codenjoy/dojo/snakebattle/client/Main.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.snakebattle.client;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,31 +23,29 @@ package com.codenjoy.dojo.snakebattle.client;
  */
 
 import com.codenjoy.dojo.client.WebSocketRunner;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URL;
-import java.util.stream.Collectors;
 
 public class Main {
+
   public static void main(String[] args) throws IOException {
     // WebSocketRunner.runClient("https://192.168.1.1:8080/codenjoy-contest/board/player/patkovskyi@gmail.com?code=6001978481505125210",
-    // WebSocketRunner.runClient("https://game2.epam-bot-challenge.com.ua/codenjoy-contest/board/player/patkovskyi@gmail.com?code=6001978481505125210",
-    //                new ClosestBestSolver(new RandomDice()),
-    //                new ClosestBestBoard());
+//    WebSocketRunner.runClient(
+//        "https://game3.epam-bot-challenge.com.ua/codenjoy-contest/board/player/pzpz4ael0yhbfcvoghei?code=7636115390628474462",
+//        new MySolver(),
+//        new MyBoard());
 
-    // String urlString = "https://game2.epam-bot-challenge.com.ua/codenjoy-contest/rest/player/njmbrnksebyfo8n9q7xiwwekm3foksnw/6001978481505125210/reset";
-    String urlString = "https://snakebattle.tk/codenjoy-contest/rest/player/patkovskyi@unreal.com/16412357891426752377/reset";
-    URL url = new URL(urlString);
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
-    System.out.println("Content: " + bufferedReader.lines().collect(Collectors.joining("\n")));
-    bufferedReader.close();
+//    String urlString = "https://game3.epam-bot-challenge.com.ua/codenjoy-contest/rest/player/pzpz4ael0yhbfcvoghei/7636115390628474462/reset";
+//    // String urlString = "https://snakebattle.tk/codenjoy-contest/rest/player/patkovskyi@unreal.com/16412357891426752377/reset";
+//    URL url = new URL(urlString);
+//    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+//    System.out.println("Content: " + bufferedReader.lines().collect(Collectors.joining("\n")));
+//    bufferedReader.close();
 
     WebSocketRunner.run(
         URI.create(
-            // "wss://game2.epam-bot-challenge.com.ua/codenjoy-contest/ws?user=patkovskyi@gmail.com&code=6001978481505125210"),
-            "wss://snakebattle.tk/codenjoy-contest/ws?user=patkovskyi@unreal.com&code=16412357891426752377"),
+            "wss://game3.epam-bot-challenge.com.ua/codenjoy-contest/ws?user=pzpz4ael0yhbfcvoghei&code=7636115390628474462"),
+            // "wss://snakebattle.tk/codenjoy-contest/ws?user=patkovskyi@unreal.com&code=16412357891426752377"),
         new MySolver(),
         new MyBoard(),
         1000);
